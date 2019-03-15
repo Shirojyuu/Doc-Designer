74f9b58e39198ca0f02a2f4736927c217b7e71f0
Alec Day <shirojyuu@gmail.com>
2019-03-15 17:16:50 -0400

    Can now add sections to projects

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,7 @@ const _ = require('lodash');
 const {Composer} = require('./Composer.js');
 const {EventBus} = require('./EventBus.js');
 
+//TODO: Adding a section to a project.
 const Sidebar = {
     name: "Sidebar",
     components: {
@@ -11,7 +12,10 @@ const Sidebar = {
         return {
             eventBus: EventBus,
             nextUri: 2,
+            isAddingNewSection: false,
+            nextSectionName: '',
             showSections: false,
+            displayedProject: {},
             displayedSections: [],
             currentContent: {},
             selectedId: '',
@@ -34,7 +38,7 @@ const Sidebar = {
                     description: '2D Stealth Platformer',
                     author: 'Alec Day',
                     sections: [
-                        {id: "QxxzA", content: {title: "Characters"}}
+                        {id: "QxxzA", content: {title: "Characters", text: ""}}
                     ]
                 },
             ]
@@ -51,8 +55,9 @@ const Sidebar = {
         }
     },
     methods: {
-        displaySections (toDisplay) {
-            this.displayedSections = toDisplay;
+        displaySections (project) {
+            this.displayedProject = project;
+            this.displayedSections = project.sections;
             this.showSections = true;
         },
         
@@ -76,9 +81,23 @@ const Sidebar = {
             project.uri = this.nextUri++;
             console.log(project);
             this.allDocs.push(project);
+        },
+        prepareNewSection () {
+            console.log('preparing')
+            this.isAddingNewSection = true;
+            this.$refs.newSec.focus();
+        },
+        addNewSection () {
+            this.isAddingNewSection = false;
+            this.displayedProject.sections.push({
+                id: this.genId(),
+                content: {title: this.nextSectionName, text: ""}
+            });
+            this.nextSectionName = '';
         }
     }, 
     mounted () {
+        //EventBus catches IPC and processes it.
         EventBus.$on('add-project', (projectData) => {
             this.addProject(projectData);
         })
@@ -87,7 +106,7 @@ const Sidebar = {
     <div class="sidebar-container">
         <div class="control-column">
             <ul id="docList" class="doc-list">
-                <li class="doc-bubble" v-for="d in allDocs" :key="d.uri" v-bind:doc="d" v-on:click="displaySections(d.sections)">
+                <li class="doc-bubble" v-for="d in allDocs" :key="d.uri" v-bind:doc="d" v-on:click="displaySections(d)">
                     <span class="doc-bubble-abbrev">{{d.abbrev}}</span>
                 </li>
                 <div class="doc-bubble add" @click="addNewDocument()">
@@ -97,10 +116,14 @@ const Sidebar = {
         </div>
         <div class="detail-column" v-bind:class="{'shown': showSections}">
             <ul id="sectionList" class="section-list">
+                <button class="sect-button" @click="prepareNewSection">New Section</button>
                 <li class="section-item" v-for="s in displayedSections" :key="s.id" 
                 v-on:click="setSection(s)" v-bind:style="{'selected': isSectionSelected}">
                     <span class="detail-list-item">{{s.content.title}}</span>
                 </li>
+                <div id="newSection" class="section-item" v-show="isAddingNewSection">
+                    <input ref="newSec" id="newSectionInput" class="userInput" type="text" v-model="nextSectionName" v-on:keyup.enter="addNewSection">
+                </div>
             </ul>
         </div>
         <composer v-bind:show="!(_.isEmpty(currentContent))" :key="selectedId" v-model="currentContent"></composer>
@@ -108,6 +131,5 @@ const Sidebar = {
     `
 }
 
-//EventBus catches IPC and processes it.
 
 module.exports = {Sidebar};
